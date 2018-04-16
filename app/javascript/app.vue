56e761a600e1d0c9d1342037868aa7dee176d2e6
ebrugulec <ebru@ebrugulec.com>
2018-04-16 15:03:37 +0300

    list draggable function added

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,24 +1,86 @@
 <template>
-  <div id="app" class="row">
-    <div v-for="list in original_lists" class="col-3">
-      {{ list.name }}
+  <draggable v-model="lists" :options="{group: 'lists'}" class="row dragArea" @end="listMoved">
+    <div v-for="(list, index) in lists" class="col-3">
+      <h5>{{ list.name }}</h5>
       <hr />
-      <div v-for="(card, index) in list.cards" class="card card-body">
-        {{ card.name }}
+      <draggable v-model="list.cards" :options="{group: 'cards'}" class="dragArea" @change="cardMoved">
+        <div v-for="(card, index) in list.cards" class="card card-body mb-3">
+          {{ card.name }}
+        </div>
+      </draggable>
+
+      <div class="card card-body">
+        <textarea v-model="messages[list.id]" class="form-control"></textarea>
+        <button v-on:click="submitMessages(list.id)" class="btn btn-secondary">Add</button>
       </div>
     </div>
-  </div>
+  </draggable>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 export default {
-  props: ["original_lists"]
+
+  components: { draggable },
+
+  props: ["original_lists"],
+
+  data: function(){
+    return {
+      messages: {},
+      lists: this.original_lists,
+    }
+  },
+
+  methods: {
+
+    listMoved: function(event) {
+      var data = {
+        position: event.newIndex + 1
+      }
+
+      $.ajax({
+        url: `lists/${this.lists[event.newIndex].id}/move`,
+        type: "PATCH",
+        data: JSON.stringify(data),
+        dataType: "json",
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+            'Content-Type': 'application/json'
+         }
+      })
+    },
+    submitMessages: function(list_id) {
+      var data = {
+        list_id: list_id,
+        name: this.messages[list_id]
+      }
+      console.log(JSON.stringify(data))
+      $.ajax({
+        url: "/cards",
+        type: "POST",
+        data: JSON.stringify(data),
+        dataType: "json",
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+            'Content-Type': 'application/json'
+         },
+        success: (data) => {
+          const index = this.lists.findIndex(item => item.id == list_id);
+          this.lists[index].cards.push(data);
+          this.messages[list_id] = undefined;
+        },
+        error: function(xhr, textStatus, errorThrown){
+           console.log('request failed');
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
+.dragArea{
+  min-height: 10px;
 }
 </style>
