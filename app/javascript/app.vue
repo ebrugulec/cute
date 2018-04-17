ecf3ee06d6624fde04c4a3c07f66574876aef6fa
ebrugulec <ebru@ebrugulec.com>
2018-04-18 00:03:26 +0300

    get and set method added, changed style

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -27,9 +27,14 @@ export default {
   },
 
   computed: {
-    lists(){
-      return this.$store.state.lists
-    }
+    lists: {
+      get() {
+        return this.$store.state.lists
+      },
+      set(value) {
+        this.$store.state.lists = value
+      },
+    },
   },
 
   methods: {
